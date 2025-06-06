87ad46eea78e24168dd7d0d44af095407ecf85a3
ReyAlas-as <alasasrey5@gmail.com>
2025-06-06 21:37:21 +0800

    Activity 4 - Authentication (Login)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 @extends('base')
 @section('title', 'Welcome Page')
 
+<a href="{{ route('auth.logout') }}" class="btn btn-danger" style="float: right;">Logout</a>
+
 <div class="centered-div">
     <div class="container">
 
